6f38d28dc82fb27f311d207a0b1e5f3902ad9bdb
lumin <182624040406@qq.com>
2024-09-02 14:36:02 +0800

    benchmark testing and some notes

---
+++ stringContact.go
@@ -0,0 +1,12 @@
+package main
+
+import "math/rand"
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn()]
+	}
+}
